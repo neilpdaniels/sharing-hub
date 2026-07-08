2ae4d7470717ca1977ff1ef015d5dab5fc5e2038
Neil <Neil_p_daniels@hotmail.com>
2026-07-08 19:37:47 +0100

    prod deploy

--- rentalution/settings/base.py
+++ rentalution/settings/base.py
@@ -129,7 +129,7 @@ INSTALLED_APPS = [
     # 'django_otp.plugins.otp_totp',
     'admin_honeypot',
     'memcache_status',
-    'django_summernote.apps.SummernoteConfig',
+    'django_summernote.apps.DjangoSummernoteConfig',
     # 'floppyforms',
     # 'material',
     # 'material.frontend',
